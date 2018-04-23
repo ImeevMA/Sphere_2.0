798685c9ca30420cedd3a69171df23abd4c69d11
Mergen Imeev <imeevma@gmail.com>
2018-04-23 17:29:00 +0300

    HW08

--- I/cpp/08/hw08.cpp
+++ I/cpp/08/hw08.cpp
@@ -1,23 +1,28 @@
 #include <algorithm>
+#include <stdexcept>
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <set>
 #include <map>
 
-void word_counter(const std::string &fname);
-
 constexpr size_t ARGNUM = 2;
+constexpr size_t TOP_N = 10;
+
+class wrong_argument_number: public std::exception {};
+class couldnt_open_file: public std::exception {};
+
+void word_counter(const std::string &fname);
 
 int main(int argc, char **argv)
 {
     try {
         if(argc != ARGNUM) {
-            throw 1;
+            throw wrong_argument_number();
         }
         word_counter(argv[1]);
     } catch(...) {
         std::cerr << "error" << std::endl;
-        return -1;
+        return 1;
     }
     return 0;
 }
@@ -26,24 +31,21 @@ void word_counter(const std::string &fname)
 {
     std::ifstream fin(fname);
     if (!fin) {
-        throw 2;
+        throw couldnt_open_file();
     }
     std::map<std::string, int> counter;
     while (fin.good()) {
         std::string word;
         fin >> word;
-        if(counter.find(word) != counter.end()) {
-            counter[word]++;
-        } else {
-            counter[word] = 1;
-        }
+        counter[word]++;
     }
-    std::vector<std::pair<int, std::string>> res;
+
+    std::set<std::pair<int, std::string>> res;
     for(auto &elem : counter) {
-        res.emplace_back(elem.second, elem.first);
+        res.emplace(elem.second, elem.first);
     }
-    std::sort(res.begin(), res.end(), std::greater<std::pair<int, std::string>>());
-    for(auto &elem : res) {
-        std::cout << elem.first << ": " << elem.second << std::endl;
+    auto it = res.rbegin();
+    for(int i = 0; i < TOP_N && it != res.rend(); ++i, ++it) {
+        std::cout << it->first << " " << it->second << std::endl;
     }
 }
